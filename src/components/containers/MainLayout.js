0e695ed8d69b12224d7eda4bcbba2311bbe72760
Lindell <tylerlindell@gmail.com>
2016-12-16 16:31:37 -0800

    adding link for stop page

--- src/components/containers/MainLayout.js
+++ src/components/containers/MainLayout.js
@@ -13,32 +13,33 @@ class MainLayout extends Component {
 		            <span className="icon-bar"></span>
 		            <span className="icon-bar"></span>
 		          </button>
-		          <a className="navbar-brand" href="#">Ground Station</a>
+		          <a className="navbar-brand" href="/">Ground Station</a>
 		        </div>
 		        <div id="navbar" className="navbar-collapse collapse">
 		          <ul className="nav navbar-nav">
-		            <li className="overview"><a href="#">Overview</a></li>
+		            <li className="overview"><a href="/">Overview</a></li>
 		            <li className="dropdown">
-		              <a href="#powerA" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Power A <span className="caret"></span></a>
+		              <a href="/powerA" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Power A <span className="caret"></span></a>
 		              <ul className="dropdown-menu">
-		                <li><a href="#powerAVoltage">Voltage</a></li>
-		                <li><a href="#powerACharger">Charger</a></li>
-		                <li><a href="#powerABMS1">BMS 1</a></li>
-		                <li><a href="#powerABMS2">BMS 2</a></li>
-		                <li><a href="#powerABMS3">BMS 3</a></li>
+		                <li><a href="/powerAVoltage">Voltage</a></li>
+		                <li><a href="/powerACharger">Charger</a></li>
+		                <li><a href="/powerABMS1">BMS 1</a></li>
+		                <li><a href="/powerABMS2">BMS 2</a></li>
+		                <li><a href="/powerABMS3">BMS 3</a></li>
 		              </ul>
 		            </li>
 		            <li className="dropdown">
-		              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Power B <span className="caret"></span></a>
+		              <a href="/" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Power B <span className="caret"></span></a>
 		              <ul className="dropdown-menu">
-		                <li><a href="#powerBVoltage">Voltage</a></li>
-		                <li><a href="#powerB">Charger</a></li>
-		                <li><a href="#powerBBMS1">BMS 1</a></li>
-		                <li><a href="#powerBBMS2">BMS 2</a></li>
-		                <li><a href="#powerBBMS3">BMS 3</a></li>
+		                <li><a href="/powerBVoltage">Voltage</a></li>
+		                <li><a href="/powerB">Charger</a></li>
+		                <li><a href="/powerBBMS1">BMS 1</a></li>
+		                <li><a href="/powerBBMS2">BMS 2</a></li>
+		                <li><a href="/powerBBMS3">BMS 3</a></li>
 		              </ul>
 		            </li>
-		            <li><a href="#lcu">LCU</a></li>
+		            <li><a href="/lcu">LCU</a></li>
+		            <li><a href="/stop">STOP</a></li>
 		          </ul>
 		        </div>
 		      </div>
