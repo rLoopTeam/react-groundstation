5eb08ca1f5976dd22c4587f81418b6b39d6be710
Tyler Lindell <tylerlindell@gmail.com>
2016-12-25 05:50:29 -0800

    moving config file into the config dir and renaming to commConfig. refactring the code in pod dir. adding dir for webpack in config dir and moving env & webpack.config in there.

--- pod/index.js
+++ pod/index.js
@@ -4,30 +4,6 @@
 * This file contains a server which can send and receive data via udp.
 * Using this, we can simulate data coming and going from the pod.
 */
-var dgram = require('dgram');
-var udpServer = dgram.createSocket('udp4');
-var config = require('../config');
-
-/*
-* UDP data sender
-*/
-function udpTX(){
-	var message = new Buffer('Telemetry data ');
-	var client = dgram.createSocket('udp4');
-	client.send(message, 0, message.length, config.GroundstationPort, config.GroundstationHost, function(err, bytes) {
-	    if (err) throw err;
-	    console.log("POD - SENT: " + config.GroundstationHost + ':' + config.GroundstationPort +' - ' + message);
-	    client.close();
-	});
-}
-
-/*
-* UDP data receiver
-*/
-udpServer.on('message', function (message, remote) {
-    console.log("POD - RECEIVED: " + remote.address + ':' + remote.port +' - ' + message);
-});
-
-udpServer.bind(config.PodPort, config.PodHost);
-
+var tx = require('./tx');
+var rx = require('./rx');
 
