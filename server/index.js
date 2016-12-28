6923c9b39a341cac3c7ec1af384d236442b37763
Piense <piense@gmail.com>
2016-12-28 13:39:55 -0800

    add real time data store to server index.js

--- server/index.js
+++ server/index.js
@@ -20,13 +20,6 @@ const server = app.listen(PORT, () => {
 
 const io = require('socket.io')(server);
 
-/*------------
-	All UDP I/O directly to/from pod.
-	***commands to the pod are currently down in websocketCommands, that should be abstracted out to here. 
-------------*/
-var udp = require('./udp/udpMain.js')(udp);
-
-
 var room = {
   dataLogging: 'dataLogging'
 };
@@ -39,17 +32,29 @@ var logger = require('./datalogging.js')(logger);
 
 
 /*------------
-    Stream Pipe
-	Sits between the data store and the client.
-	Sends requested parameters to the client at a fixed interval.
-------------*/
-const StreamPipeServer = require('./StreamPipeServer.js')(app, io);
-
-
-/*------------
   WEBSOCKETS
   Handles commands from the client to send to the Pod.
 ------------*/
 const websocketCommands = require('./websocketCommands.js')(io, udp, room, logger);
 
+/*------------
+	All UDP I/O directly to/from pod.
+	***commands to the pod are currently down in websocketCommands, that should be abstracted out to here. 
+------------*/
+var udp = require('./udp/udpMain.js')(udp);
 
+/*------------
+	RTDATASTORE	
+	Holds the last parameter values received from the pod.
+	Queried by the stream pipe server for data to send to the client
+------------*/
+//TODO pass a hook to connect the UDP rx with the real time data store,
+//TODO pass or return a hook to give the stream pipe server access to this data store
+var rtDataStore = require('./realtimeDataStore')(); 
+
+/*------------
+    Stream Pipe
+	Sits between the data store and the client.
+	Sends requested parameters to the client at a fixed interval.
+------------*/
+const StreamPipeServer = require('./StreamPipeServer.js')(app, io);
