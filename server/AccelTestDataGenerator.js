284344171358e80468a5612c6598b3043470523a
Paul <paullehenaff@msn.com>
2016-12-31 14:31:55 +0000

    [update] Tweaked the data generator to replicate the issue Lachlan is seeing [fix] Also changed the fault flag type to 32 bit instead of 8, to match the data format

--- server/AccelTestDataGenerator.js
+++ server/AccelTestDataGenerator.js
@@ -31,10 +31,13 @@ class AccelTestDataGenerator{
 		var payload = [];
 		
 		//Accel 1
-		payload.push.apply(payload,bin.uint8ToBytes(5,true)); //Fault Flags
-		payload.push.apply(payload,bin.int16ToBytes(20,true)); //Raw X Axis data
-		payload.push.apply(payload,bin.int16ToBytes(21,true)); //Raw Y Axis data
-		payload.push.apply(payload,bin.int16ToBytes(22,true)); //Raw Z Axis data
+		payload.push.apply(payload,bin.uint32ToBytes(5,true)); //Fault Flags
+		// payload.push.apply(payload,bin.int16ToBytes(20,true)); //Raw X Axis data
+		// payload.push.apply(payload,bin.int16ToBytes(21,true)); //Raw Y Axis data
+		// payload.push.apply(payload,bin.int16ToBytes(22,true)); //Raw Z Axis data
+		payload.push.apply(payload,bin.int16ToBytes(-100,true)); //Raw X Axis data
+		payload.push.apply(payload,bin.int16ToBytes(-666,true)); //Raw Y Axis data
+		payload.push.apply(payload,bin.int16ToBytes(1024,true)); //Raw Z Axis data
 		payload.push.apply(payload,bin.float32ToBytes(20.123,true)); //Raw X Axis data
 		payload.push.apply(payload,bin.float32ToBytes(20.123,true)); //Raw Y Axis data
 		payload.push.apply(payload,bin.float32ToBytes(20.123,true)); //Raw Z Axis data
@@ -42,16 +45,19 @@ class AccelTestDataGenerator{
 		payload.push.apply(payload,bin.float32ToBytes(60.0,true)); //Roll Angle
 
 		//Accel 2
-		payload.push.apply(payload,bin.uint8ToBytes(50,true)); //Fault Flags
-		payload.push.apply(payload,bin.int16ToBytes(200,true)); //Raw X Axis data
-		payload.push.apply(payload,bin.int16ToBytes(201,true)); //Raw Y Axis data
-		payload.push.apply(payload,bin.int16ToBytes(202,true)); //Raw Z Axis data
+		payload.push.apply(payload,bin.uint32ToBytes(50,true)); //Fault Flags
+		// payload.push.apply(payload,bin.int16ToBytes(200,true)); //Raw X Axis data
+		// payload.push.apply(payload,bin.int16ToBytes(201,true)); //Raw Y Axis data
+		// payload.push.apply(payload,bin.int16ToBytes(202,true)); //Raw Z Axis data
+		payload.push.apply(payload,bin.int16ToBytes(-2002,true)); //Raw X Axis data
+		payload.push.apply(payload,bin.int16ToBytes(-5005,true)); //Raw Y Axis data
+		payload.push.apply(payload,bin.int16ToBytes(4096,true)); //Raw Z Axis data
 		payload.push.apply(payload,bin.float32ToBytes(200.123,true)); //Raw X Axis data
 		payload.push.apply(payload,bin.float32ToBytes(200.345,true)); //Raw Y Axis data
 		payload.push.apply(payload,bin.float32ToBytes(200.678,true)); //Raw Z Axis data
 		payload.push.apply(payload,bin.float32ToBytes(500.0,true)); //Pitch Angle
 		payload.push.apply(payload,bin.float32ToBytes(600.0,true)); //Roll Angle
-	
+
 		this.packetParser.gotNewPacket(this.makeSafetyUDP(this.sequence, 0x1003, payload));
 
 		this.sequence = this.sequence + 1;
