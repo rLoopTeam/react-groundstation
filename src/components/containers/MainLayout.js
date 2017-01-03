7fe384a25892616ffcb4ca2553d6e3b1a2062c6b
Tyler Lindell <tylerlindell@gmail.com>
2017-01-02 22:47:59 -0800

    cleaning up code for links in the header

--- src/components/containers/MainLayout.js
+++ src/components/containers/MainLayout.js
@@ -3,84 +3,115 @@ import { Link } from 'react-router';
 import InfoPanel from '../InfoPanel';
 
 class MainLayout extends Component {
+	constructor(){
+		super()
+
+		this.links = [
+			{
+				name: "Overview", location: "/"
+			},
+			{
+				name: "Power A", location: "/powerA", children: [
+					{name: "Voltage", location: "/powerAVoltage"},
+					{name: "Charger", location: "/powerACharger"},
+					{name: "BMS 1", location: "/powerABMS1"},
+					{name: "BMS 2", location: "/powerABMS2"},
+					{name: "BMS 3", location: "/powerABMS3"},
+				]
+			},
+			{
+				name: "Power B", location: "/powerB", children: [
+					{name: "Voltage", location: "/powerBVoltage"},
+					{name: "Charger", location: "/powerBCharger"},
+					{name: "BMS 1", location: "/powerBBMS1"},
+					{name: "BMS 2", location: "/powerBBMS2"},
+					{name: "BMS 3", location: "/powerBBMS3"},
+				]
+			},
+			{
+				name: "LCU", location: "/lcu"
+			},
+			{
+				name: "LGU", location: "/lgu"
+			},
+			{
+				name: "STOP", location: "/stop"
+			},
+			{
+				name: "Flight Control", location: "/flightControl", children: [
+					{name: "Accelerometers", location: "/flightcontrol_accel"},
+				]
+			},
+			{
+				name: "Extras", location: "/extras", children: [
+					{name: "Xilinx Sim", location: "/xilinxsim"},
+				]
+			},
+			{
+				name: "Config", location: "/config", children: [
+					{name: "Comms", location: "/commConfig"},
+				]
+			},
+		];
+	}
+
 	render() {
 	    return (
         <div className="container-fluid">
 	      	<nav className="navbar navbar-inverse navbar-fixed-top">
-						<div className="col-xs-12">
-							<div className="navbar-header">
-								<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-									<span className="sr-only">Toggle navigation</span>
-									<span className="icon-bar"></span>
-									<span className="icon-bar"></span>
-									<span className="icon-bar"></span>
-								</button>
-								<Link className="navbar-brand" to="/">Ground Station</Link>
-							</div>
-							<div id="navbar" className="navbar-collapse collapse">
-								<ul className="nav navbar-nav">
-									<li className="overview"><Link to="/">Overview</Link></li>
-									<li className="dropdown">
-										<Link to="/powerA" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Power A <span className="caret"></span></Link>
-										<ul className="dropdown-menu">
-											<li><Link to="/powerAVoltage">Voltage</Link></li>
-											<li><Link to="/powerACharger">Charger</Link></li>
-											<li><Link to="/powerABMS1">BMS 1</Link></li>
-											<li><Link to="/powerABMS2">BMS 2</Link></li>
-											<li><Link to="/powerABMS3">BMS 3</Link></li>
-										</ul>
-									</li>
-									<li className="dropdown">
-										<Link to="/" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Power B <span className="caret"></span></Link>
-										<ul className="dropdown-menu">
-											<li><Link to="/powerBVoltage">Voltage</Link></li>
-											<li><Link to="/powerB">Charger</Link></li>
-											<li><Link to="/powerBBMS1">BMS 1</Link></li>
-											<li><Link to="/powerBBMS2">BMS 2</Link></li>
-											<li><Link to="/powerBBMS3">BMS 3</Link></li>
-										</ul>
-									</li>
-									<li><Link to="/lcu">LCU</Link></li>
-									<li><Link to="/lgu">LGU</Link></li>
-									<li><Link to="/stop">STOP</Link></li>
-
-						<li className="dropdown">
-										<Link to="/" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Flight Control<span className="caret"></span></Link>
-										<ul className="dropdown-menu">
-							<li><Link to="/flightcontrol_accel">Accelerometers</Link></li>
-										</ul>
-									</li>
-						
-									<li className="dropdown">
-										<Link to="/" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Extras<span className="caret"></span></Link>
-										<ul className="dropdown-menu">
-							<li><Link to="/xilinxsim">Xilinx Sim</Link></li>
-										</ul>
-									</li>
-						
-						<li className="streamExample"><Link to="/datasubexample">Data Stream Example</Link></li>
-						<li className="dropdown">
-								<Link to="/" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Config<span className="caret"></span></Link>
-								<ul className="dropdown-menu">
-									<li><Link to="/commConfig">Comms</Link></li>
-								</ul>
-						</li>
-								</ul>
-							</div>
-						</div>
-					</nav>
-					<div className="row">
-						<div className="col-xs-12">
-							{this.props.children}
-						</div>
+				<div className="col-xs-12">
+					<div className="navbar-header">
+						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span className="sr-only">Toggle navigation</span>
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+							<span className="icon-bar"></span>
+						</button>
+						<Link className="navbar-brand" to="/">Ground Station</Link>
 					</div>
+					<div id="navbar" className="navbar-collapse collapse">
+						<ul className="nav navbar-nav">
+							{
+								this.links.map(function(item, index){
+									if(item.children){
+										return (
+											<li key={index} className="dropdown">
+												<Link to={item.location} className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{item.name}<span className="caret"></span></Link>
+												<ul className="dropdown-menu">
+													{
+														item.children.map(function(child, chIndex){
+															return(
+																<li key={chIndex}><Link to={child.location}>{child.name}</Link></li>
+															);
+														})
+													}
+												</ul>
+											</li>
+										);
+									}
+									else{
+										return(
+											<li key={index}><Link to={item.location}>{item.name}</Link></li>
+										);
+									}
+								})
+							}
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<div className="row">
+				<div className="col-xs-12">
+					{this.props.children}
+				</div>
+			</div>
 
-					<div className="row">
-						<div className="col-xs-12">
-								<InfoPanel />
-						</div>
+			<div className="row">
+				<div className="col-xs-12">
+						<InfoPanel />
+				</div>
 	        </div>
-		    </div>
+		</div>
 	    );
 	}
 }
