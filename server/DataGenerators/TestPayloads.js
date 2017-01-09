a8a03952ff1df8581d1dac6172aea9f6defd4dd6
Paul <paullehenaff@msn.com>
2017-01-09 00:01:05 +0000

    [fix] Updated accelerometer payload to be actually correct

--- server/DataGenerators/TestPayloads.js
+++ server/DataGenerators/TestPayloads.js
@@ -1,20 +1,30 @@
 const bin = require('../udp/binary');
+
+/*
+* 0x1003 Accelerometer test data
+*/
 var accelerometer = []
-accelerometer.push.apply(accelerometer,bin.uint8ToBytes(5,true)); //Fault Flags
-accelerometer.push.apply(accelerometer,bin.float32ToBytes(90.1,true)); //Raw X Axis data
-accelerometer.push.apply(accelerometer,bin.float32ToBytes(2.1,true)); //Raw X Axis data
-accelerometer.push.apply(accelerometer,bin.uint8ToBytes(5,true)); //Fault Flags
-accelerometer.push.apply(accelerometer,bin.uint8ToBytes(52,true)); //Fault Flags
-accelerometer.push.apply(accelerometer,bin.uint8ToBytes(51,true)); //Fault Flags
-accelerometer.push.apply(accelerometer,bin.uint8ToBytes(25,true)); //Fault Flags
-accelerometer.push.apply(accelerometer,bin.float32ToBytes(12.34,true)); //Raw X Axis data
-accelerometer.push.apply(accelerometer,bin.float32ToBytes(120.1,true)); //Raw X Axis data
-accelerometer.push.apply(accelerometer,bin.float32ToBytes(3.2,true)); //Raw X Axis data
-accelerometer.push.apply(accelerometer,bin.float32ToBytes(2.4,true)); //Raw X Axis data
+//Accel 1
+accelerometer.push.apply(accelerometer,bin.uint32ToBytes(5,true)); //Fault Flags
+accelerometer.push.apply(accelerometer,bin.int16ToBytes(-100,true)); //Raw X Axis data
+accelerometer.push.apply(accelerometer,bin.int16ToBytes(-666,true)); //Raw Y Axis data
 accelerometer.push.apply(accelerometer,bin.int16ToBytes(1024,true)); //Raw Z Axis data
-accelerometer.push.apply(accelerometer,bin.int16ToBytes(23,true)); //Raw Z Axis data
-accelerometer.push.apply(accelerometer,bin.int16ToBytes(124,true)); //Raw Z Axis data
-accelerometer.push.apply(accelerometer,bin.int16ToBytes(7024,true)); //Raw Z Axis data
+accelerometer.push.apply(accelerometer,bin.float32ToBytes(20.123,true)); //Raw X Axis data
+accelerometer.push.apply(accelerometer,bin.float32ToBytes(20.123,true)); //Raw Y Axis data
+accelerometer.push.apply(accelerometer,bin.float32ToBytes(20.123,true)); //Raw Z Axis data
+accelerometer.push.apply(accelerometer,bin.float32ToBytes(60.0,true)); //Pitch Angle
+accelerometer.push.apply(accelerometer,bin.float32ToBytes(60.0,true)); //Roll Angle
+
+//Accel 2
+accelerometer.push.apply(accelerometer,bin.uint32ToBytes(50,true)); //Fault Flags
+accelerometer.push.apply(accelerometer,bin.int16ToBytes(-2002,true)); //Raw X Axis data
+accelerometer.push.apply(accelerometer,bin.int16ToBytes(-5005,true)); //Raw Y Axis data
+accelerometer.push.apply(accelerometer,bin.int16ToBytes(4096,true)); //Raw Z Axis data
+accelerometer.push.apply(accelerometer,bin.float32ToBytes(200.123,true)); //Raw X Axis data
+accelerometer.push.apply(accelerometer,bin.float32ToBytes(200.345,true)); //Raw Y Axis data
+accelerometer.push.apply(accelerometer,bin.float32ToBytes(200.678,true)); //Raw Z Axis data
+accelerometer.push.apply(accelerometer,bin.float32ToBytes(500.0,true)); //Pitch Angle
+accelerometer.push.apply(accelerometer,bin.float32ToBytes(600.0,true)); //Roll Angle
 
 module.exports = {
 	accelerometer
