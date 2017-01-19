2d68e30d9a4c6eef891fa3f15b9d354ba729a282
Tyler Lindell <tylerlindell@gmail.com>
2017-01-18 22:24:01 -0800

    getting D3 line chart to work

--- src/components/charts/D3LineChart.js
+++ src/components/charts/D3LineChart.js
@@ -1,63 +1,61 @@
 // first of course react!
 import React, {Component} from 'react';
-// require `react-d3-core` for Chart component, which help us build a blank svg and chart title.
-import {Chart} from 'react-d3-core';
-// require `react-d3-basic` for Line chart component.
-import {LineChart} from 'react-d3-basic';
+import rd3 from 'react-d3';
 
+var LineChart = rd3.LineChart;
 
 class D3LineChart extends Component{
     constructor(props){
         super(props)
 
-        this.chartData = [
-            {
-                date: new Date(),
-                value1: parseInt(Math.random()*10, 10),
-                value2: parseInt(Math.random()*10, 10),
-            }
-        ]
 
-        this.width = 700;
-            this.height = 300;
-            this.margins = {left: 100, right: 100, top: 50, bottom: 50};
-            this.title = "User sample";
-            // chart series,
-            // field: is what field your data want to be selected
-            // name: the name of the field that display in legend
-            // color: what color is the line
-            this.chartSeries = [
-                {
-                    field: 'BMI',
-                    name: 'BMI',
-                    color: '#ff7f0e'
-                }
-            ];
-            // your x accessor
-            this.x = function(d) {
-                return d.index;
+        this.lineData = [
+            {
+                name: "series1",
+                values: [{x: Date.now(), y: Math.random()*10}],
+                strokeWidth: 3,
+                strokeDashArray: "5,5",
+            },
+            {
+                name: "series2",
+                values: [{x: Date.now(), y: Math.random()*10}],
             }
+        ];
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            var vals1 = this.lineData[0].values;
+            var vals2 = this.lineData[1].values;
+            if(vals1.length > 30)
+                vals1.splice(0, 1);
+            if(vals2.length > 30)
+                vals2.splice(0, 1);
+            vals1.push({x: Date.now(), y: Math.random()*10})
+            vals2.push({x: Date.now(), y: Math.random()*10})
+            this.forceUpdate()
+        }, 100);
     }
 
     render()
     {
         return(
-            <Chart
-                title={this.title}
-                width={this.width}
-                height={this.height}
-                margins={this.margins}
-                >
-                <LineChart
-                    margins={this.margins}
-                    title={this.title}
-                    data={this.chartData}
-                    width={this.width}
-                    height={this.height}
-                    chartSeries={this.chartSeries}
-                    x={this.x}
+            <LineChart
+                legend={true}
+                data={this.lineData}
+                width={1100}
+                height={400}
+                viewBoxObject={{
+                    x: 0,
+                    y: 0,
+                    width: 1100,
+                    height: 400
+                }}
+                title="Line Chart"
+                yAxisLabel="Altitude"
+                xAxisLabel="Elapsed Time (sec)"
+                gridHorizontal={true}
                 />
-            </Chart>
         );
     }
 }
