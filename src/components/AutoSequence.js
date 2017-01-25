bdb36c2f1c5d2d6d4e1bf38f630504f8153a1c73
nikhil <nikhil@nikhil.cc>
2017-01-25 02:11:46 -0800

    Require each successive auto-sequence test to appear before a timeout

--- src/components/AutoSequence.js
+++ src/components/AutoSequence.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import GenericParameterInput from './GenericParameterInput.js';
 import StreamingPageManager from '../StreamingPageManager.js';
 import _ from 'lodash';
+import defer from 'defer-promise';
+
+// Max milliseconds between any two test results
+// (before we give up on all remaining test results).
+const TEST_TIMEOUT = 10000;
 
 class AutoSequence extends Component {
 
@@ -44,43 +48,85 @@ class AutoSequence extends Component {
 				state: 0x0008,
 				status: '?',
 			}],
+
+			// Added by this.scheduleNextTestResult
+			//nextTestResult: defer()
 		};
 
-		this.state.streamingPageManager.RequestPacketWithCallback('Auto-sequence test', this.dataCallback.bind(this));
-		this._isMounted = true;
+		this.scheduleNextTestResult(null);
+
+		this.state.streamingPageManager.RequestPacketWithCallback(
+			'Auto-sequence test',
+			this.state.nextTestResult.resolve);
+
+		//this._isMounted = true;
 	}
 
 	componentWillUnmount() {
-		this._isMounted = false;
+		// TODO
+		//this._isMounted = false;
 		this.state.streamingPageManager.destroy();
 	}
 
-	dataCallback(newPacket){
-		// TODO: fix this gating logic, assuming it's needed
-		//if (this._isMounted) {
-			var newPacketState = 0x0001;
-	        var newPacketStatus = '?';
-			newPacket.parameters.forEach(function(parameter) {
-				if (parameter.name === 'Auto-test State') {
-					newPacketState = parameter.value;
-				}
-				if (parameter.name === 'Auto-test Status') {
-					newPacketStatus = !!parameter.value ? 'Pass' : 'Fail';
-				}
-			});
+	/**
+	 * @param {number|null} timeout: Time period (ms) in which the next test result must appear
+	 *                               or else we'll give up on all pending tests.
+	 */
+	scheduleNextTestResult(timeout) {
+		this.state.nextTestResult = defer(); // manually resolved when the next packet arrives
+		let nextTestResultDeadline = new Promise((resolve, reject) => {
+			if (timeout) {
+				setTimeout(reject, timeout);
+			}
+		})
+		Promise.race([
+			this.state.nextTestResult.promise,
+			nextTestResultDeadline
+		]).then(
+			this.recordTestResult.bind(this),
+			this.markAllPendingTestResultsAsFailed.bind(this)
+		);
+	}
 
-			var newComponentState = _.cloneDeep(this.state);
-			newComponentState.testResults = this.state.testResults.map(function(testResult) {
-				if (testResult.state === newPacketState) {
-					return _.assign({}, testResult, {
-						status: newPacketStatus
-					});
-				} else {
-					return testResult;
-				}
-			});
-			this.setState(newComponentState);
-		//}
+	recordTestResult(newPacket) {
+		var newPacketState = 0x0001;
+		var newPacketStatus = '?';
+		newPacket.parameters.forEach(function(parameter) {
+			if (parameter.name === 'Auto-test State') {
+				newPacketState = parameter.value;
+			}
+			if (parameter.name === 'Auto-test Status') {
+				newPacketStatus = !!parameter.value ? 'Pass' : 'Fail';
+			}
+		});
+
+		var newComponentState = _.cloneDeep(this.state);
+		newComponentState.testResults = this.state.testResults.map(function(testResult) {
+			if (testResult.state === newPacketState) {
+				return _.assign({}, testResult, {
+					status: newPacketStatus,
+				});
+			} else {
+				return testResult;
+			}
+		});
+		this.setState(newComponentState);
+
+		this.scheduleNextTestResult(TEST_TIMEOUT);
+	}
+
+	markAllPendingTestResultsAsFailed() {
+		var newComponentState = _.cloneDeep(this.state);
+		newComponentState.testResults = this.state.testResults.map(function(testResult) {
+			if (testResult.status === '?') {
+				return _.assign({}, testResult, {
+					status: 'Timeout',
+				});
+			} else {
+				return testResult;
+			}
+		});
+		this.setState(newComponentState);
 	}
 
 	getTestResults() {
@@ -92,6 +138,7 @@ class AutoSequence extends Component {
 			'?': 'test-result-unknown',
 			'Pass': 'test-result-passed',
 			'Fail': 'test-result-failed',
+			'Timeout': 'test-result-failed',
 		}[status];
 	}
 
