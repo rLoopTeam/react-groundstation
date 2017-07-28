4a56b5fa1d179af6ba4503bbd2e1eb4ed30e4160
nepeat <nepeat@users.noreply.github.com>
2017-07-28 04:02:23 -0700

    StreamPipeClient changes for the single socket model.

--- src/StreamPipeClient.js
+++ src/StreamPipeClient.js
@@ -1,34 +1,60 @@
-import io from 'socket.io-client';
+import createSocket from './shared/socket';
 
 class DataStreamClient {
 
   constructor (newPacketCallback) {
     this.value = 0;
     this.RequestedParameters = [];
+    this.newPacketCallback = newPacketCallback;
 
-    this.connected = false;
+    this.initialRequest = this.initialRequest.bind(this);
+    this.handleDataBurst = this.handleDataBurst.bind(this);
 
-    this.socket = io('/dataStreamServer', {'forceNew': true});
-
-    this.socket.on('connect', function () {
-      this.connected = true;
-
-      this.socket.emit('request parameters', this.RequestedParameters);
-
-      this.socket.on('new data burst', function (msg, fn) {
-        newPacketCallback(msg);
-        fn('');
-      });
-
-      this.socket.on('disconnect', function () { this.connected = false; });
-    }.bind(this));
+    this.socket = createSocket('dataStream', '/dataStreamServer');
+    this.openSocket();
   }
 
   RequestParameter (parameter) {
     this.RequestedParameters.push(parameter);
-    if (this.connected) {
+    if (this.socket.connected) {
       this.socket.emit('request parameter', parameter);
     }
+  }
+
+  initialRequest () {
+    console.log('initials');
+    this.socket.emit('request parameters', this.RequestedParameters);
+  }
+
+  handleDataBurst (msg, fn) {
+    this.newPacketCallback(msg);
+    fn('');
+  }
+
+  /**
+   * Opens a socket.
+   * This doesn't actually open a socket because of the single shared "dataStream" socket.
+   * Instead, this adds our handlers to the socket.
+   */
+  openSocket () {
+    this.socket.on('new data burst', this.handleDataBurst);
+    this.socket.on('connect', this.initialRequest);
+  }
+
+  /**
+   * "Closes" the socket.
+   * This doesn't actually close the socket because of the single shared "dataStream" socket.
+   * Instead, this removes our handlers on the socket.
+   */
+  closeSocket (options) {
+    // Clears all parameters this client is listening to.
+    // Use caution with this flag because it will also stop the parameter bursts for other clients.   
+    if (options.clearParameters) {
+      this.socket.emit('stop parameters', this.RequestedParameters);
+      this.RequestedParameters = [];
+    }
+    this.socket.removeListener('new data burst', this.handleDataBurst);
+    this.socket.removeListener('connection', this.initialRequest);
   }
 }
 
