2755f5d5c420ce7c63b61430a6065e91604674d1
nuttwerx <nuttwerx@users.noreply.github.com>
2017-08-14 18:19:49 -0700

    Create nominalConditions.js

---
+++ config/nominalConditions.js
@@ -0,0 +1,128 @@
+module.exports = {
+  "Power A BMS":
+  {
+    "Average Temp":
+    {
+      "Faults": false,
+      "Min": 0,
+      "Max": 50
+    },
+    "Highest Sensor Value":
+    {
+      "Faults": false,
+      "Min": 0,
+      "Max": 50
+    },
+    "Pack Volts":
+    {
+      "Faults": false,
+      "Min": 62.1,
+      "Max": 75.6
+    },
+    "Highest Cell Volts":
+    {
+      "Faults": false,
+      "Min": 3.45,
+      "Max": 4.2
+    },
+    "Lowest Cell Volts":
+    {
+      "Faults": false,
+      "Min": 3.45,
+      "Max": 4.2
+    },
+    "Pack Current":
+    {
+      "Faults": false,
+      "Min": 0,
+      "Max": 550
+    },
+    "Node Pressure":
+    {
+      "Faults": false,
+      "Min": 0.7,
+      "Max": 1.1
+    },
+    "Node Temp":
+    {
+      "Faults": false,
+      "Min": 0,
+      "Max": 40
+    }
+  },
+  "Power B BMS":
+  {
+    "Average Temp":
+    {
+      "Faults": false,
+      "Min": 0,
+      "Max": 50
+    },
+    "Highest Sensor Value":
+    {
+      "Faults": false,
+      "Min": 0,
+      "Max": 50
+    },
+    "Pack Volts":
+    {
+      "Faults": false,
+      "Min": 62.1,
+      "Max": 75.6
+    },
+    "Highest Cell Volts":
+    {
+      "Faults": false,
+      "Min": 3.45,
+      "Max": 4.2
+    },
+    "Lowest Cell Volts":
+    {
+      "Faults": false,
+      "Min": 3.45,
+      "Max": 4.2
+    },
+    "Pack Current":
+    {
+      "Faults": false,
+      "Min": 0,
+      "Max": 550
+    },
+    "Node Pressure":
+    {
+      "Faults": false,
+      "Min": 0.7,
+      "Max": 1.1
+    },
+    "Node Temp":
+    {
+      "Faults": false,
+      "Min": 0,
+      "Max": 40
+    }
+  },
+  "Accel Data Full":
+  {
+    "flags":{
+      "Faults": true
+    },
+    "X Gs":
+    {
+      "Faults": false,
+      "Min": -3,
+      "Max": 3
+    },
+    "Y Gs":
+    {
+      "Faults": false,
+      "Min": -3,
+      "Max": 3
+    },
+    "Z Gs":
+    {
+      "Faults": false,
+      "Min": -3,
+      "Max": 3
+    }
+  }
+};
