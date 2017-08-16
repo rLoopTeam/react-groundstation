fd7f312d739944cbcdee98d6ba0cb37cf2c2b1d9
nepeat <nepeat@users.noreply.github.com>
2017-08-15 20:51:48 -0700

    nominalConditions, fix indentation.

--- config/nominalConditions.js
+++ config/nominalConditions.js
@@ -1,251 +1,251 @@
 module.exports = {
-    "Power A BMS": {
-        "Average Temp": {
-            "Fault": false,
-            "Min": 0,
-            "Max": 50
-        },
-        "Highest Sensor Value": {
-            "Fault": false,
-            "Min": 0,
-            "Max": 50
-        },
-        "Pack Volts": {
-            "Fault": false,
-            "Min": 62.1,
-            "Max": 75.6
-        },
-        "Highest Cell Volts": {
-            "Fault": false,
-            "Min": 3.45,
-            "Max": 4.2
-        },
-        "Lowest Cell Volts": {
-            "Fault": false,
-            "Min": 3.45,
-            "Max": 4.2
-        },
-        "Pack Current": {
-            "Fault": false,
-            "Min": 0,
-            "Max": 550
-        },
-        "Node Pressure": {
-            "Fault": false,
-            "Min": 0.7,
-            "Max": 1.1
-        },
-        "Node Temp": {
-            "Fault": false,
-            "Min": 0,
-            "Max": 40
-        }
+  'Power A BMS': {
+    'Average Temp': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 50
     },
-    "Power B BMS": {
-        "Average Temp": {
-            "Fault": false,
-            "Min": 0,
-            "Max": 50
-        },
-        "Highest Sensor Value": {
-            "Fault": false,
-            "Min": 0,
-            "Max": 50
-        },
-        "Pack Volts": {
-            "Fault": false,
-            "Min": 62.1,
-            "Max": 75.6
-        },
-        "Highest Cell Volts": {
-            "Fault": false,
-            "Min": 3.45,
-            "Max": 4.2
-        },
-        "Lowest Cell Volts": {
-            "Fault": false,
-            "Min": 3.45,
-            "Max": 4.2
-        },
-        "Pack Current": {
-            "Fault": false,
-            "Min": 0,
-            "Max": 550
-        },
-        "Node Pressure": {
-            "Fault": false,
-            "Min": 0.7,
-            "Max": 1.1
-        },
-        "Node Temp": {
-            "Fault": false,
-            "Min": 0,
-            "Max": 40
-        }
+    'Highest Sensor Value': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 50
     },
-    "Accel 1": {
-        "Flags": {
-            "Fault": true
-        },
-        "X Gs": {
-            "Fault": false,
-            "Min": -3,
-            "Max": 3
-        },
-        "Y Gs": {
-            "Fault": false,
-            "Min": -3,
-            "Max": 3
-        },
-        "Z Gs": {
-            "Fault": false,
-            "Min": -3,
-            "Max": 3
-        }
+    'Pack Volts': {
+      'Fault': false,
+      'Min': 62.1,
+      'Max': 75.6
     },
-    "Accel 2": {
-      "Flags": {
-          "Fault": true
-      },
-      "X Gs": {
-          "Fault": false,
-          "Min": -3,
-          "Max": 3
-      },
-      "Y Gs": {
-          "Fault": false,
-          "Min": -3,
-          "Max": 3
-      },
-      "Z Gs": {
-          "Fault": false,
-          "Min": -3,
-          "Max": 3
-      }
-  },
-    "ForwardLaser": {
-        "Fault Flags": {
-            "Fault": true
-        },
-        "Distance": {
-            "Min": 0,
-            "Max": 300000
-        }
+    'Highest Cell Volts': {
+      'Fault': false,
+      'Min': 3.45,
+      'Max': 4.2
     },
-    "LaserOpto": {
-        "Fault Flags": {
-            "Fault": true
-        }
+    'Lowest Cell Volts': {
+      'Fault': false,
+      'Min': 3.45,
+      'Max': 4.2
     },
-    "LaserContrast0": {
-        "System Fault Flags": {
-            "Fault": true
-        }
+    'Pack Current': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 550
     },
-    "LaserContrast1": {
-        "System Fault Flags": {
-            "Fault": true
-        }
+    'Node Pressure': {
+      'Fault': false,
+      'Min': 0.7,
+      'Max': 1.1
     },
-    "LaserContrast2": {
-        "System Fault Flags": {
-            "Fault": true
-        }
-    },
-    "Brake": {
-        "Calibration State": {
-            "Fault": true
-        },
-        "State": {
-            "Fault": true
-        },
-        "SW Error 1": {
-            "Fault": true
-        },
-        "SW Error 2": {
-            "Fault": true
-        }
-    },
-    "ASI": {
-      "1 Temperature": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 80
-      },
-      "2 Temperature": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 80
-      },
-      "3 Temperature": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 80
-      },
-      "4 Temperature": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 80
-      },
-      "5 Temperature": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 80
-      },
-      "6 Temperature": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 80
-      },
-      "7 Temperature": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 80
-      },
-      "8 Temperature": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 80
-      },
-      "1 HE RPM": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 3000
-      },
-      "2 HE RPM": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 3000
-      },
-      "3 HE RPM": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 3000
-      },
-      "4 HE RPM": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 3000
-      },
-      "5 HE RPM": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 3000
-      },
-      "6 HE RPM": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 3000
-      },
-      "7 HE RPM": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 3000
-      },
-      "8 HE RPM": {
-        "Fault": false,
-        "Min": 0,
-        "Max": 3000
-      }
+    'Node Temp': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 40
     }
+  },
+  'Power B BMS': {
+    'Average Temp': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 50
+    },
+    'Highest Sensor Value': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 50
+    },
+    'Pack Volts': {
+      'Fault': false,
+      'Min': 62.1,
+      'Max': 75.6
+    },
+    'Highest Cell Volts': {
+      'Fault': false,
+      'Min': 3.45,
+      'Max': 4.2
+    },
+    'Lowest Cell Volts': {
+      'Fault': false,
+      'Min': 3.45,
+      'Max': 4.2
+    },
+    'Pack Current': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 550
+    },
+    'Node Pressure': {
+      'Fault': false,
+      'Min': 0.7,
+      'Max': 1.1
+    },
+    'Node Temp': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 40
+    }
+  },
+  'Accel 1': {
+    'Flags': {
+      'Fault': true
+    },
+    'X Gs': {
+      'Fault': false,
+      'Min': -3,
+      'Max': 3
+    },
+    'Y Gs': {
+      'Fault': false,
+      'Min': -3,
+      'Max': 3
+    },
+    'Z Gs': {
+      'Fault': false,
+      'Min': -3,
+      'Max': 3
+    }
+  },
+  'Accel 2': {
+    'Flags': {
+      'Fault': true
+    },
+    'X Gs': {
+      'Fault': false,
+      'Min': -3,
+      'Max': 3
+    },
+    'Y Gs': {
+      'Fault': false,
+      'Min': -3,
+      'Max': 3
+    },
+    'Z Gs': {
+      'Fault': false,
+      'Min': -3,
+      'Max': 3
+    }
+  },
+  'ForwardLaser': {
+    'Fault Flags': {
+      'Fault': true
+    },
+    'Distance': {
+      'Min': 0,
+      'Max': 300000
+    }
+  },
+  'LaserOpto': {
+    'Fault Flags': {
+      'Fault': true
+    }
+  },
+  'LaserContrast0': {
+    'System Fault Flags': {
+      'Fault': true
+    }
+  },
+  'LaserContrast1': {
+    'System Fault Flags': {
+      'Fault': true
+    }
+  },
+  'LaserContrast2': {
+    'System Fault Flags': {
+      'Fault': true
+    }
+  },
+  'Brake': {
+    'Calibration State': {
+      'Fault': true
+    },
+    'State': {
+      'Fault': true
+    },
+    'SW Error 1': {
+      'Fault': true
+    },
+    'SW Error 2': {
+      'Fault': true
+    }
+  },
+  'ASI': {
+    '1 Temperature': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 80
+    },
+    '2 Temperature': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 80
+    },
+    '3 Temperature': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 80
+    },
+    '4 Temperature': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 80
+    },
+    '5 Temperature': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 80
+    },
+    '6 Temperature': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 80
+    },
+    '7 Temperature': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 80
+    },
+    '8 Temperature': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 80
+    },
+    '1 HE RPM': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 3000
+    },
+    '2 HE RPM': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 3000
+    },
+    '3 HE RPM': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 3000
+    },
+    '4 HE RPM': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 3000
+    },
+    '5 HE RPM': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 3000
+    },
+    '6 HE RPM': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 3000
+    },
+    '7 HE RPM': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 3000
+    },
+    '8 HE RPM': {
+      'Fault': false,
+      'Min': 0,
+      'Max': 3000
+    }
+  }
 };
