0358d8db35ccdfbc153eedafbf12000d66ac987d
nepeat <nepeat@users.noreply.github.com>
2017-08-21 03:41:51 -0700

    Move state machine constants to shared constants.

--- src/components/StateMachine.js
+++ src/components/StateMachine.js
@@ -5,65 +5,11 @@ import EnumStatusDisplay from './EnumStatusDisplay.js';
 import ConfirmButton from './buttons/ConfirmButton.js';
 
 import createSocket from '../shared/socket';
+import { STATEMACHINE_STATES, STATEMACHINE_STATES_INT_INDEXED, STATEMACHINE_TRANSITIONS } from '../shared/constants';
 
 import './StateMachine.css';
 
 let socket = createSocket();
-
-const STATEMACHINE_STATES = {
-  UNKNOWN_STATE: '?',
-  NO_COMMAND: 0,
-  IDLE: 1,
-  TEST_MODE: 2,
-  DRIVE: 3,
-  ARMED_WAIT: 4,
-  FLIGHT_PREP: 5,
-  READY: 6,
-  ACCEL: 7,
-  COAST_INTERLOCK: 8,
-  BRAKE: 9,
-  SPINDOWN: 10
-};
-
-const STATEMACHINE_STATES_INT_INDEXED = {
-  '?': 'UNKNOWN_STATE',
-  0: 'NO_COMMAND',
-  1: 'IDLE',
-  2: 'TEST_MODE',
-  3: 'DRIVE',
-  4: 'ARMED_WAIT',
-  5: 'FLIGHT_PREP',
-  6: 'READY',
-  7: 'ACCEL',
-  8: 'COAST_INTERLOCK',
-  9: 'BRAKE',
-  10: 'SPINDOWN'
-};
-
-const STATEMACHINE_TRANSITIONS = {
-  'IDLE': [
-    'TEST_MODE',
-    'DRIVE',
-    'ARMED_WAIT'
-  ],
-  'TEST_MODE': [
-    'IDLE'
-  ],
-  'DRIVE': [
-    'IDLE'
-  ],
-  'ARMED_WAIT': [
-    'IDLE',
-    'FLIGHT_PREP'
-  ],
-  'FLIGHT_PREP': [
-    'ARMED_WAIT',
-    'READY'
-  ],
-  'READY': [
-    'FLIGHT_PREP'
-  ]
-};
 
 class StateMachine extends Component {
   constructor (props) {
