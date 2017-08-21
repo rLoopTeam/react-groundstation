f5b81c469c536bf619b4b1a1063760563b60da4e
nuttwerx <nuttwerx@users.noreply.github.com>
2017-08-20 18:54:35 -0700

    Update nominalConditions.js
    changed how we access the nominal numbers

--- config/nominalConditions.js
+++ config/nominalConditions.js
@@ -1,177 +1,90 @@
 module.exports = {
-    "Power A BMS Average Temp": {fault: false, min: 0, max: 50}
-    ,
-    "Power A BMS Highest Sensor Value": {fault: false, min: 0, max: 50}
-    ,
-    "Power A BMS Pack Volts": {fault: false, min: 62.1, max: 75.6}
-    ,
-    "Power A BMS Highest Cell Volts": {fault: false, min: 3.45, max: 4.2}
-    ,
-    "Power A BMS Lowest Cell Volts": {fault: false, min: 3.45, max: 4.2}
-    ,
-    "Power A BMS Pack Current": {fault: false, min: 0, max: 550}
-    ,
-    "Power A BMS Node Pressure": {fault: false, min: 0.7, max: 1.1}
-    ,
-    "Power A BMS Node Temp": {fault: false, min: 0, max: 40}
-    ,
-    "Power B BMS Average Temp": {fault: false, min: 0, max: 50}
-    ,
-    "Power B BMS Highest Sensor Value": {fault: false, min: 0, max: 50}
-    ,
-    "Power B BMS Pack Volts": {fault: false, min: 62.1, max: 75.6}
-    ,
-    "Power B BMS Highest Cell Volts": {fault: false, min: 3.45, max: 4.2}
-    ,
-    "Power B BMS Lowest Cell Volts": {fault: false, min: 3.45, max: 4.2}
-    ,
-    "Power B BMS Pack Current": {fault: false, min: 0, max: 550}
-    ,
-    "Power B BMS Node Pressure": {fault: false, min: 0.7, max: 1.1}
-    ,
-    "Power B BMS Node Temp": {fault: false, min: 0, max: 40}
-    ,
-    "Accel 1 Flags": {fault: true}
-    ,
-    "Accel 1 X Gs": {fault: false, min: -3, max: 3}
-    ,
-    "Accel 1 Y Gs": {fault: false, min: -3, max: 3}
-    ,
-    "Accel 1 Z Gs": {fault: false, min: -3, max: 3}
-    ,
-    "Accel 2 Flags": {fault: true}
-    ,
-    "Accel 2 X Gs": {fault: false, min: -3, max: 3}
-    ,
-    "Accel 2 Y Gs": {fault: false, min: -3, max: 3}
-    ,
-    "Accel 2 Z Gs": {fault: false, min: -3, max: 3}
-    ,
-    "ForwardLaser Fault Flags": {fault: true}
-    ,
-    "ForwardLaser Distance": {min: 0, max: 300000}
-    ,
-    "LaserOpto Fault Flags": {fault: true}
-    ,
-    "LaserOpto 1 Filtered value": {fault: false, min: 6, max: 20}
-    ,
-    "LaserOpto 2 Filtered value": {fault: false, min: 6, max: 20}
-    ,
-    "LaserOpto 3 Filtered value": {fault: false, min: 6, max: 20}
-    ,
-    "LaserOpto 4 Filtered value": {fault: false, min: 6, max: 20}
-    ,
-    "LaserOpto 5 Filtered value": {fault: false, min: 20, max: 30}
-    ,
-    "LaserOpto 6 Filtered value": {fault: false, min: 20, max: 30}
-    ,
-    "LaserContrast0 System Fault Flags": {fault: true}
-    ,
-    "LaserContrast1 System Fault Flags": {fault: true}
-    ,
-    "LaserContrast2 System Fault Flags": {fault: true}
-    ,
-    "Brake Calibration State": {fault: false, min: 6, max: 6}
-    ,
-    "Brake State": {fault: false, min: 0, max: 5}
-    ,
-    "Brake SW Error 1": {fault: true}
-    ,
-    "Brake SW Error 2": {fault: true}
-    ,
-    "Brake MLP 1 Current": {fault: false, min: 0, max: 75}
-    ,
-    "Brake MLP 2 Current": {fault: false, min: 0, max: 75}
-    ,
-    "Brake Limit Extend 1": {fault: false, min: 1, max: 2}
-    ,
-    "Brake Limit Retract 1": {fault: false, min: 1, max: 2}
-    ,
-    "Brake Limit Extend Edge 1": {fault: false, min: 1, max: 2}
-    ,
-    "Brake Limit Retract Edge 1": {fault: false, min: 1, max: 2}
-    ,
-    "Brake Limit Extend 2": {fault: false, min: 1, max: 2}
-    ,
-    "Brake Limit Retract 2": {fault: false, min: 1, max: 2}
-    ,
-    "Brake Limit Extend Edge 2": {fault: false, min: 1, max: 2}
-    ,
-    "Brake Limit Retract Edge 2": {fault: false, min: 1, max: 2}
-    ,
-    "ASI 1 Temperature": {fault: false, min: 0, max: 80}
-    ,
-    "ASI 2 Temperature": {fault: false, min: 0, max: 80}
-    ,
-    "ASI 3 Temperature": {fault: false, min: 0, max: 80}
-    ,
-    "ASI 4 Temperature": {fault: false, min: 0, max: 80}
-    ,
-    "ASI 5 Temperature": {fault: false, min: 0, max: 80}
-    ,
-    "ASI 6 Temperature": {fault: false, min: 0, max: 80}
-    ,
-    "ASI 7 Temperature": {fault: false, min: 0, max: 80}
-    ,
-    "ASI 8 Temperature": {fault: false, min: 0, max: 80}
-    ,
-    "ASI 1 HE RPM": {fault: false, min: 0, max: 3000}
-    ,
-    "ASI 2 HE RPM": {fault: false, min: 0, max: 3000}
-    ,
-    "ASI 3 HE RPM": {fault: false, min: 0, max: 3000}
-    ,
-    "ASI 4 HE RPM": {fault: false, min: 0, max: 3000}
-    ,
-    "ASI 5 HE RPM": {fault: false, min: 0, max: 3000}
-    ,
-    "ASI 6 HE RPM": {fault: false, min: 0, max: 3000}
-    ,
-    "ASI 7 HE RPM": {fault: false, min: 0, max: 3000}
-    ,
-    "ASI 8 HE RPM": {fault: false, min: 0, max: 3000}
-    ,
-    "ASI 1 Motor Current": {fault: false, min: 0, max: 70}
-    ,
-    "ASI 2 Motor Current": {fault: false, min: 0, max: 70}
-    ,
-    "ASI 3 Motor Current": {fault: false, min: 0, max: 70}
-    ,
-    "ASI 4 Motor Current": {fault: false, min: 0, max: 70}
-    ,
-    "ASI 5 Motor Current": {fault: false, min: 0, max: 70}
-    ,
-    "ASI 6 Motor Current": {fault: false, min: 0, max: 70}
-    ,
-    "ASI 7 Motor Current": {fault: false, min: 0, max: 70}
-    ,
-    "ASI 8 Motor Current": {fault: false, min: 0, max: 70}
-    ,
-    "Pusher Switch State 1": {fault: false, min: 0, max: 1}
-    ,
-    "Pusher Switch State 2": {fault: false, min: 0, max: 1}
-    ,
-    "LGU Computed Height 1": {fault: false, min: 5, max: 17}
-    ,
-    "LGU Computed Height 2": {fault: false, min: 5, max: 17}
-    ,
-    "LGU Computed Height 3": {fault: false, min: 5, max: 17}
-    ,
-    "LGU Computed Height 4": {fault: false, min: 5, max: 17}
-    ,
-    "LGU Switch Extend 1": {fault: false, min: 1, max: 2}
-    ,
-    "LGU Switch Extend 2": {fault: false, min: 1, max: 2}
-    ,
-    "LGU Switch Extend 3": {fault: false, min: 1, max: 2}
-    ,
-    "LGU Switch Extend 4": {fault: false, min: 1, max: 2}
-    ,
-    "LGU Switch Retract 1": {fault: false, min: 1, max: 2}
-    ,
-    "LGU Switch Retract 2": {fault: false, min: 1, max: 2}
-    ,
-    "LGU Switch Retract 3": {fault: false, min: 1, max: 2}
-    ,
+    "Power A BMS Average Temp": {fault: false, min: 0, max: 50},
+    "Power A BMS Highest Sensor Value": {fault: false, min: 0, max: 50},
+    "Power A BMS Pack Volts": {fault: false, min: 62.1, max: 75.6},
+    "Power A BMS Highest Cell Volts": {fault: false, min: 3.45, max: 4.2},
+    "Power A BMS Lowest Cell Volts": {fault: false, min: 3.45, max: 4.2},
+    "Power A BMS Pack Current": {fault: false, min: 0, max: 550},
+    "Power A BMS Node Pressure": {fault: false, min: 0.7, max: 1.1},
+    "Power A BMS Node Temp": {fault: false, min: 0, max: 40},
+    "Power B BMS Average Temp": {fault: false, min: 0, max: 50},
+    "Power B BMS Highest Sensor Value": {fault: false, min: 0, max: 50},
+    "Power B BMS Pack Volts": {fault: false, min: 62.1, max: 75.6},
+    "Power B BMS Highest Cell Volts": {fault: false, min: 3.45, max: 4.2},
+    "Power B BMS Lowest Cell Volts": {fault: false, min: 3.45, max: 4.2},
+    "Power B BMS Pack Current": {fault: false, min: 0, max: 550},
+    "Power B BMS Node Pressure": {fault: false, min: 0.7, max: 1.1},
+    "Power B BMS Node Temp": {fault: false, min: 0, max: 40},
+    "Accel 1 Flags": {fault: true},
+    "Accel 1 X Gs": {fault: false, min: -3, max: 3},
+    "Accel 1 Y Gs": {fault: false, min: -3, max: 3},
+    "Accel 1 Z Gs": {fault: false, min: -3, max: 3},
+    "Accel 2 Flags": {fault: true},
+    "Accel 2 X Gs": {fault: false, min: -3, max: 3},
+    "Accel 2 Y Gs": {fault: false, min: -3, max: 3},
+    "Accel 2 Z Gs": {fault: false, min: -3, max: 3},
+    "ForwardLaser Fault Flags": {fault: true},
+    "ForwardLaser Distance": {min: 0, max: 300000},
+    "LaserOpto Fault Flags": {fault: true},
+    "LaserOpto 1 Filtered value": {fault: false, min: 6, max: 20},
+    "LaserOpto 2 Filtered value": {fault: false, min: 6, max: 20},
+    "LaserOpto 3 Filtered value": {fault: false, min: 6, max: 20},
+    "LaserOpto 4 Filtered value": {fault: false, min: 6, max: 20},
+    "LaserOpto 5 Filtered value": {fault: false, min: 20, max: 30},
+    "LaserOpto 6 Filtered value": {fault: false, min: 20, max: 30},
+    "LaserContrast0 System Fault Flags": {fault: true},
+    "LaserContrast1 System Fault Flags": {fault: true},
+    "LaserContrast2 System Fault Flags": {fault: true},
+    "Brake Calibration State": {fault: false, min: 6, max: 6},
+    "Brake State": {fault: false, min: 0, max: 5},
+    "Brake SW Error 1": {fault: true},
+    "Brake SW Error 2": {fault: true},
+    "Brake MLP 1 Current": {fault: false, min: 0, max: 75},
+    "Brake MLP 2 Current": {fault: false, min: 0, max: 75},
+    "Brake Limit Extend 1": {fault: false, min: 1, max: 2},
+    "Brake Limit Retract 1": {fault: false, min: 1, max: 2},
+    "Brake Limit Extend Edge 1": {fault: false, min: 1, max: 2},
+    "Brake Limit Retract Edge 1": {fault: false, min: 1, max: 2},
+    "Brake Limit Extend 2": {fault: false, min: 1, max: 2},
+    "Brake Limit Retract 2": {fault: false, min: 1, max: 2},
+    "Brake Limit Extend Edge 2": {fault: false, min: 1, max: 2},
+    "Brake Limit Retract Edge 2": {fault: false, min: 1, max: 2},
+    "ASI 1 Temperature": {fault: false, min: 0, max: 80},
+    "ASI 2 Temperature": {fault: false, min: 0, max: 80},
+    "ASI 3 Temperature": {fault: false, min: 0, max: 80},
+    "ASI 4 Temperature": {fault: false, min: 0, max: 80},
+    "ASI 5 Temperature": {fault: false, min: 0, max: 80},
+    "ASI 6 Temperature": {fault: false, min: 0, max: 80},
+    "ASI 7 Temperature": {fault: false, min: 0, max: 80},
+    "ASI 8 Temperature": {fault: false, min: 0, max: 80},
+    "ASI 1 HE RPM": {fault: false, min: 0, max: 3000},
+    "ASI 2 HE RPM": {fault: false, min: 0, max: 3000},
+    "ASI 3 HE RPM": {fault: false, min: 0, max: 3000},
+    "ASI 4 HE RPM": {fault: false, min: 0, max: 3000},
+    "ASI 5 HE RPM": {fault: false, min: 0, max: 3000},
+    "ASI 6 HE RPM": {fault: false, min: 0, max: 3000},
+    "ASI 7 HE RPM": {fault: false, min: 0, max: 3000},
+    "ASI 8 HE RPM": {fault: false, min: 0, max: 3000},
+    "ASI 1 Motor Current": {fault: false, min: 0, max: 70},
+    "ASI 2 Motor Current": {fault: false, min: 0, max: 70},
+    "ASI 3 Motor Current": {fault: false, min: 0, max: 70},
+    "ASI 4 Motor Current": {fault: false, min: 0, max: 70},
+    "ASI 5 Motor Current": {fault: false, min: 0, max: 70},
+    "ASI 6 Motor Current": {fault: false, min: 0, max: 70},
+    "ASI 7 Motor Current": {fault: false, min: 0, max: 70},
+    "ASI 8 Motor Current": {fault: false, min: 0, max: 70},
+    "Pusher Switch State 1": {fault: false, min: 0, max: 1},
+    "Pusher Switch State 2": {fault: false, min: 0, max: 1},
+    "LGU Computed Height 1": {fault: false, min: 5, max: 17},
+    "LGU Computed Height 2": {fault: false, min: 5, max: 17},
+    "LGU Computed Height 3": {fault: false, min: 5, max: 17},
+    "LGU Computed Height 4": {fault: false, min: 5, max: 17},
+    "LGU Switch Extend 1": {fault: false, min: 1, max: 2},
+    "LGU Switch Extend 2": {fault: false, min: 1, max: 2},
+    "LGU Switch Extend 3": {fault: false, min: 1, max: 2},
+    "LGU Switch Extend 4": {fault: false, min: 1, max: 2},
+    "LGU Switch Retract 1": {fault: false, min: 1, max: 2},
+    "LGU Switch Retract 2": {fault: false, min: 1, max: 2},
+    "LGU Switch Retract 3": {fault: false, min: 1, max: 2},
     "LGU Switch Retract 4": {fault: false, min: 1, max: 2}
 };
