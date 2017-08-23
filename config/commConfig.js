340b92e8e1e20399c206c4bafca53a431ceca83c
nuttwerx <nofel.tiani@gmail.com>
2017-08-23 13:28:05 -0700

    definitions update

--- config/commConfig.js
+++ config/commConfig.js
@@ -1,34 +1,68 @@
 module.exports = {
-  Appserver: {
-    port: 3000,
-    ip: '127.0.0.1'
+  "Appserver": {
+    "port": 3000,
+    "ip": "127.0.0.1"
   },
-  // Used for testing purposes
-  PodRxPort: 9100, // Pod details
-  PodRxHost: '127.0.0.1',
-  testDataGeneratorTargetHost: '127.0.0.1',
-  testDataGeneratorTargetPort: '9900',
-  RXServers: [
-  /*
-  {'port':9110,'hostIP':'192.168.0.110','hostName':'Power Node A'},
-  {'port':9111,'hostIP':'192.168.0.111','hostName':'Power Node B'},
-  {'port':9100,'hostIP':'192.168.0.100','hostName':'Flight Control'},
-  {'port':9120,'hostIP':'192.168.0.120','hostName':'Landing Gear'},
-  {'port':9130,'hostIP':'192.168.0.130','hostName':'Gimbal Control'},
-  {'port':9170,'hostIP':'192.168.1.170','hostName':'Xilinx Sim'} */
-
-  // http://confluence.rloop.org/display/SD/Static+IP+Assignments
-  {'port': 9110, 'hostIP': '192.168.0.255', 'hostName': 'Power Node A'},
-  {'port': 9111, 'hostIP': '192.168.0.255', 'hostName': 'Power Node B'},
-  {'port': 9531, 'hostIP': '192.168.0.255', 'hostName': 'Flight Control'},
-  {'port': 9548, 'hostIP': '192.168.0.255', 'hostName': 'Landing Gear'},
-  {'port': 9619, 'hostIP': '192.168.0.255', 'hostName': 'Gimbal Control'},
-  {'port': 9170, 'hostIP': '192.168.0.255', 'hostName': 'Xilinx Sim'},
-  {'port': 9608, 'hostIP': '192.168.0.255', 'hostName': 'HE Thermal Monitor'},
-  {'port': 9616, 'hostIP': '192.168.0.255', 'hostName': 'ASI Monitor System'},
-  {'port': 9615, 'hostIP': '192.168.0.255', 'hostName': 'Aux Prop Controller'},
-  {'port': 9631, 'hostIP': '192.168.0.255', 'hostName': 'Charger'},
-  {'port': 9633, 'hostIP': '192.168.0.255', 'hostName': 'IPS Charger'}  
+  "PodRxPort": 9100,
+  "PodRxHost": "127.0.0.1",
+  "testDataGeneratorTargetHost": "127.0.0.1",
+  "testDataGeneratorTargetPort": "9900",
+  "RXServers": [
+    {
+      "port": 9110,
+      "hostIP": "192.168.0.255",
+      "hostName": "Power Node A"
+    },
+    {
+      "port": 9111,
+      "hostIP": "192.168.0.255",
+      "hostName": "Power Node B"
+    },
+    {
+      "port": 9531,
+      "hostIP": "192.168.0.255",
+      "hostName": "Flight Control"
+    },
+    {
+      "port": 9548,
+      "hostIP": "192.168.0.255",
+      "hostName": "Landing Gear"
+    },
+    {
+      "port": 9619,
+      "hostIP": "192.168.0.255",
+      "hostName": "Gimbal Control"
+    },
+    {
+      "port": 9170,
+      "hostIP": "192.168.0.255",
+      "hostName": "Xilinx Sim"
+    },
+    {
+      "port": 9608,
+      "hostIP": "192.168.1.255",
+      "hostName": "HE Thermal Monitor"
+    },
+    {
+      "port": 9531,
+      "hostIP": "192.168.0.255",
+      "hostName": "ASI Monitor System"
+    },
+    {
+      "port": 9615,
+      "hostIP": "192.168.0.255",
+      "hostName": "Aux Prop Controller"
+    },
+    {
+      "port": 9631,
+      "hostIP": "192.168.0.255",
+      "hostName": "Charger"
+    },
+    {
+      "port": 9633,
+      "hostIP": "192.168.0.255",
+      "hostName": "IPS Charger"
+    }
   ],
-  MirrorLocal: true
-};
+  "MirrorLocal": true
+}
